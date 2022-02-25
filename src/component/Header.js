46a83f255bd64b1d575392c39f0f4d18cb10c1e5
lolu1023 <soowkw@naver.com>
2022-02-25 15:30:45 +0900

    8일차 컴포넌트 나누기?

---
+++ src/component/Header.js
@@ -0,0 +1,41 @@
+import {FaUser,FaHeart,FaShoppingBag} from "react-icons/fa"
+
+function head() {
+  return (
+    <>
+      <div className='header_menu'>
+        <div className='head_container'>
+          <ul>
+            <li>도움말</li>
+            <li>반품</li>
+            <li>주문조회</li>
+            <li>아디클럽 가입하기</li>
+            <li>아디클럽</li>
+            <li>로그인</li>
+          </ul>
+        </div>
+      </div>
+      <div className='header'>
+        <div className='header_cont'>
+          <div>
+            <img className='header_logo' src='https://www.adidas.co.kr/glass/react/5152538/assets/img/icon-adidas-logo.svg' alt='adidas' />
+          </div>
+          <ul>
+            <li>MEN</li>
+            <li>WOMEN</li>
+            <li>KIDS</li>
+            <li>SPORTS</li>
+            <li>BRANDS</li>
+            <li>OUTLET</li>
+          </ul>
+          <ul>
+            <li><FaUser size={20} /></li>
+            <li><FaHeart  size={20}/></li>
+            <li><FaShoppingBag size={20} /></li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+export default head
